48b499813c78b69caf5271ba51bcc8bc050379a9
ddouglss <ddouglss1999@gmail.com>
2025-06-01 13:43:11 -0300

    endpoints criados e respondendo corretamente

--- modules/express.js
+++ modules/express.js
@@ -1,24 +1,31 @@
 const express = require('express');
-
+const UserModel= require('../src/models/user.model');
 const app = express()
 
+app.use(express.json());
 app.get("/home", (req, res) => {
     res.contentType('application/html');
     res.status(200).send("<h1>Hello World</h1>");
 });
 
-app.get("/users", (req, res) =>{
-    const users = [
-        {
-            name: 'Gregory Drew',
-            email: 'gregory@drew.com'
-        },
-        {
-            name: 'Charlote Does',
-            email: 'gharlote@does.com'
-        },
-    ];
-    res.status(200).json(users);
+app.get("/users", async (req, res) =>{
+    try {
+        const users = await UserModel.find({})
+
+        res.status(200).json(users);
+    }catch (err) {
+        res.status(500).send(err.message);
+    }
+});
+
+app.post("/users",  async (req, res) => {
+    try{
+        const user =  await UserModel.create(req.body)
+
+        res.status(201).json(user);
+    } catch(err){
+        res.status(500).send(err.message)
+    }
 })
 
 const port = 8080;
